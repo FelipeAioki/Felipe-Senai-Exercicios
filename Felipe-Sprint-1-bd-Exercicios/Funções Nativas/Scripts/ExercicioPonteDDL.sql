f432ca82fd7bcf3def9e2c6b036bbd8b19d7c89a
FelipeAioki <73197986+FelipeAioki@users.noreply.github.com>
2021-03-08 09:23:54 -0300

    Add files via upload

---
+++ Felipe-Sprint-1-bd-Exercicios/Funções Nativas/Scripts/ExercicioPonteDDL.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE ExercicioPonte;
+
+USE ExercicioPonte;
+
+CREATE TABLE Ponte
+(
+	HIP FLOAT 
+	,CO FLOAT
+	,CA FLOAT
+);
---
+++ Felipe-Sprint-1-bd-Exercicios/Funções Nativas/Scripts/ExercicioPonteDQL.sql
@@ -0,0 +1,18 @@
+--60²= 10²+C²
+--hip = 60
+--co = 10
+--ca = ?
+
+
+SELECT SQUARE(60) AS quadradoHip
+SELECT SQUARE(10) AS quadradoCo
+
+-- 60²-10² = 3500
+SELECT 3600-100
+
+
+-- Raiz de 3500 = CA
+SELECT SQRT (3500)  
+
+
+SELECT * FROM Ponte;
