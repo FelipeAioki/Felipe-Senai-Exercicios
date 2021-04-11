234fec2c53f76fbac65ac81ae47b74939d159a00
FelipeAioki <73197986+FelipeAioki@users.noreply.github.com>
2021-04-11 16:41:19 -0300

    Add files via upload

---
+++ Felipe-Sprint-1-bd-Exercicios/M_Peoples/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Domains/funcionariosDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Peoples.WebApi.Domains
+{
+    public class funcionariosDomain
+    {
+        public int idNome { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string nascimento { get; set; }
+    }
+}
---
+++ Felipe-Sprint-1-bd-Exercicios/M_Peoples/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,36 @@
+﻿using Senai.Peoples.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Peoples.WebApi.Interfaces
+{
+    /// <summary>
+    /// Interface == O que vai ser feito
+    /// Repositorio == Como vai ser feito
+    /// </summary>
+    public interface IFuncionarioRepository
+    {
+        //TipoDeRetorno NomeMetodo (Paramêtros);
+        //Void          Cadastrar  ();
+        //Retorna todos os funcionarios
+        List<funcionariosDomain> ListarTodos();
+
+
+        //Busca um funcionario atraves do id
+        funcionariosDomain BuscarPorId(int id);
+
+        //Cadastra um funcionario com os atributos contidos no domain
+        void Cadastrar(funcionariosDomain novoFuncionario);
+
+        //Atualiza o funcionario ultilizando o Id pelo corpo da requisição
+        void AtualizarIdCorpo(funcionariosDomain funcionarios);
+
+        //Atualiza o funcionario ultilizando o id pela URL
+        void AtualizarIdUrl(int id, funcionariosDomain funcionarios);
+
+        //Deleta um funcionario pela URL
+        void Deletar(int id);
+    }
+}
