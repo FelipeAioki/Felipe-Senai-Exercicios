37c151b5e00257ff777a62fe70628c6a718ab52c
felipe queiroz <felipe300040@gmail.com>
2021-04-26 09:42:48 -0300

    solução criada

---
+++ Felipe-Sprint-2-exercicios/Hroads/senai.HRoads.WebApi/senai.HRoads.WebApi/Domains/tipoUsuarioDomain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.HRoads.WebApi.Domains
+{
+    public class tipoUsuarioDomain
+    {
+    }
+}
